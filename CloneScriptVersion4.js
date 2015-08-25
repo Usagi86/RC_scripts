bcc8120487fa12b4aefc3db6ce41a33383809077
Usagi86 <Usagi86@users.noreply.github.com>
2015-08-25 16:20:16 -0600

    Update CloneScriptVersion4.js

--- CloneScriptVersion4.js
+++ CloneScriptVersion4.js
@@ -209,4 +209,15 @@ function newColor10()
 }
 //Fin de nuevos colores.
 
+//+ NUEVA EDICION DE ESTILOS:
+// Fondos:
+document.getElementById("WRP").style.background="rgb(0,0,0)"
+document.getElementsByClassName("pN")[0].style.background = 'rgba(44,175,226,1)'
+document.getElementsByClassName("pN")[1].style.background = 'rgba(44,175,226,0.8)'
+document.getElementsByClassName("pN")[2].style.background = 'rgba(44,175,226,0.8)'
+// PL, PC (lista de participantes):
+document.getElementById("PL").style.color='#FFFFFF';
+document.getElementById("PC").style.color='#FFFFFF';
+//Fin_de_edicion_de_estilos.
+
 //FIN DEL SCRIPT.

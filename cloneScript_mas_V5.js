817943308689aebbff39fd4d8191f0ef72c09bd3
Usagi86 <Usagi86@users.noreply.github.com>
2015-11-17 18:00:28 -0600

    Update cloneScript_mas_V5.js

--- cloneScript_mas_V5.js
+++ cloneScript_mas_V5.js
@@ -1,8 +1,7 @@
 /**************************
 	CloneScript Version 5
 	11/11/2015
-	(Primera versión:28/oct/2014)
-	Author: ++
+	Author: +
 **************************/
 
 
@@ -12,56 +11,28 @@ var ver = setInterval('rcFC.send("700","750")',600000);
 ver;
 //clearInterval(ver);
 
-//++ Cambiar el contenido de la ventana help y mostrar ahi los nuevos elementos:
+//++ Script para el Flooder:
+var scriptF = document.createElement('script');
+scriptF.src = 'https://rawgit.com/Usagi86/RC_scripts/master/flooder.js';
+var headF = document.getElementsByTagName('head')[0];
+headF.appendChild(scriptF);
+
+//++ Cambiar el contenido de la ventana help y mostrar ahi los nuevos elementos para el clone y para el flooder:
 document.getElementById('HDB').innerHTML = ' <a style="font-size:17px; color: #0056b2;"><b>Spam-Flooder(Usar con cuidado):<b></a><div>&nbsp;</div> <div>Texto SPAM: <input id="spamText" size="60px" title="Escribe/pega acÃ¡ el texto que quieras que se envÃ­e" style="background: #C4E7FF; border:2px solid #000000" ></input></div><div>&nbsp;</div> <div>Tiempo(milisegundos): <input id="tiempo" size="5" title="Escribe un nÃºmero a partir de 1250. Mientras mayor sea mÃ¡s tiempo tardarÃ¡." value="1250" style="background: #C4E7FF; border:2px solid #000000"><i> [nÃºmero mayor a 1250]</i></input></div> <div>&nbsp;</div><div>Iniciar/Parar:&nbsp;<button id="bts1" title="Estado del Flooder" onmouseup="presbts1();" style="cursor: pointer; border: 0.8px solid #000000; background: #C5EA35;">OFF</button></div><hr size="4" width="100%" align="left" style="color: #0056b2;"></hr><a style="font-size:20px; color: #0056b2;"><b>Clonar:</a><div>Texto a mostrar: <input id="text_in" size="40" style="background: #C4E7FF; border:2px solid #000000"></input><a>&nbsp;</a><a id="bt_enviar" onmousedown="cl_text();" class="btn" style="cursor:pointer; border:0.8px solid #000000; background: #C5EA35;"><b>Enviar </b></a><div>&nbsp;</div><div>Nick a clonar: <input id="cl_nick" style="background: #C4E7FF; border:2px solid #000000"></input><a>&nbsp;&nbsp;</a><a id="bt_b" title="Letra en Negrita" onmousedown="negrita();" class="btn" style="cursor:pointer; border:2px solid rgb(6, 65, 98); background:transparent;"><b>B </b></a><a>&nbsp;</a><a id="bt_i" title="Letra en Cursiva" onmousedown="cursiva();" class="btn" style="cursor:pointer; border:2px solid rgb(6, 65, 98); background:transparent;"><i>I </i></a><a>&nbsp;</a><a id="bt_u" title="Letra subrayada" onmousedown="subrayado();" class="btn" style="cursor:pointer; border:2px solid rgb(6, 65, 98); background:transparent;"><u>U </u></a></div><a id="mostrar" style="color: #000000;" title="Solo da Click sobre el color a elegir">Color del nick a clonar:</a><div><a id="tablaSimple" style="visibility: visible; "><a id="bt1" class="btn" onclick="ocultar()" style="cursor:pointer; background: #909090;">&nbsp;&nbsp;&nbsp;</a><a id="bt2" class="btn" onclick="ocultar()" style="cursor:pointer; background: #FF0300;">&nbsp;&nbsp;&nbsp;</a><a id="bt3" class="btn" onclick="ocultar()" style="cursor:pointer; background: #FF9A00;">&nbsp;&nbsp;&nbsp;</a><a id="bt4" class="btn" onclick="ocultar()" style="cursor:pointer; background: #FFCD00;">&nbsp;&nbsp;&nbsp;</a><a id="bt5" class="btn" onclick="ocultar()" style="cursor:pointer; background: #C5EA35;">&nbsp;&nbsp;&nbsp;</a><a id="bt6" class="btn" onclick="ocultar()" style="cursor:pointer; background: #4BE22C;">&nbsp;&nbsp;&nbsp;</a><a id="bt7" class="btn" onclick="ocultar()" style="cursor:pointer; background: #2CAFE2;">&nbsp;&nbsp;&nbsp;</a><a id="bt8" class="btn" onclick="ocultar()" style="cursor:pointer; background: #353CC4;">&nbsp;&nbsp;&nbsp;</a><a id="bt9" class="btn" onclick="ocultar()" style="cursor:pointer; background: #992CE2;">&nbsp;&nbsp;&nbsp;</a><a id="bt10" class="btn" onclick="ocultar()" style="cursor:pointer; background: #E22C9A;">&nbsp;&nbsp;&nbsp;</a></div><div><a id="bt11" class="btn" onclick="ocultar()" style="cursor:pointer; background: #000000;">&nbsp;&nbsp;&nbsp;</a><a id="bt12" class="btn" onclick="ocultar()" style="cursor:pointer; background: #CE1918;">&nbsp;&nbsp;&nbsp;</a><a id="bt13" class="btn" onclick="ocultar()" style="cursor:pointer; background: #CE5D18;">&nbsp;&nbsp;&nbsp;</a><a id="bt14" onclick="ocultar()" class="btn" style="cursor:pointer; background: #CEAB18;">&nbsp;&nbsp;&nbsp;</a><a id="bt15" class="btn" onclick="ocultar()" style="cursor:pointer; background: #97B71C;">&nbsp;&nbsp;&nbsp;</a><a id="bt16" class="btn" onclick="ocultar()" style="cursor:pointer; background: #35A31F;">&nbsp;&nbsp;&nbsp;</a><a id="bt17" class="btn" onclick="ocultar()" style="cursor:pointer; background: #1F7EA3;">&nbsp;&nbsp;&nbsp;</a><a id="bt18" class="btn" onclick="ocultar()" style="cursor:pointer; background: #1F54A3;">&nbsp;&nbsp;&nbsp;</a><a id="bt19" onclick="ocultar()" class="btn" style="cursor:pointer; background: #6E1FA3;">&nbsp;&nbsp;&nbsp;</a><a id="bt20" class="btn" onclick="ocultar()" style="cursor:pointer; background: #A31F6E;">&nbsp;&nbsp;&nbsp;</a></ul></a></div></div> ';
-
-
-//++ ScriptMultiSpam:(Flooder)
-function multiSpam()
-{
-	var myText=document.getElementById("spamText").value;
-	var us_lista = ULS.users.length;
-	var uS = Math.floor((Math.random()*us_lista)+0);
-	var u_nick=ULS.users[uS].nick;
-	var T_1="[b][c="+IN.clr+"]"+myText+"[/c][/b]";
-	var textO=T_1;
-	var chekk=ULS.getID(u_nick); 
-	if (chekk!=null) 
-	{
-		rcFC.send("500","612",chekk,textO);
-	}
-}
-
-var spamm;
-function presbts1(){
-if (document.getElementById("bts1").innerHTML=="ON..")
-	{
-		rcFC.playSound("msg");
-		document.getElementById("bts1").innerHTML="OFF";
-		clearInterval(spamm);
-	}
-else
-	{
-		rcFC.playSound("join");
-		document.getElementById("bts1").innerHTML="ON..";
-		spamm = setInterval('multiSpam();',document.getElementById("tiempo").value);
-	}
-}
-//Fin_del_MultiFlooder
 
 
 //++ ANTIGÜO ARCHIVO DE CABECERA ++//
 
-//+ variable que guardara el codigo del color del clon:
+// variable que guardara el codigo del color del clon:
 var colorN="#000000";
 
-//+funcion para ocultar el elemento tablaSimple:
-function ocultar()
+// funcion para darle al clon el color que elijamos:
+function ocultar()//(antiguo nombre de otra funcion que ocultaba un elemento)
 {
 	document.getElementById("mostrar").style.color = colorN;
 }
 
-//+ al dar click sobre los colores me elegira uno nuevo para el clon:
+// al dar click sobre los colores me elegira uno nuevo para el clon:
 document.getElementById("bt1").onclick=function() {colorN="#909090";};
 document.getElementById("bt2").onclick=function() {colorN="#FF0300";};
 document.getElementById("bt3").onclick=function() {colorN="#FF9A00";};
@@ -222,7 +193,6 @@ document.getElementById("PC").style.color='#FFFFFF';
 
 //////////////////////////////////////////////////
 //++ GENERADOR DE TEXTO (letras-caracteres):
-
 var script=document.createElement('script');
 script.src='https://rawgit.com/Usagi86/RC_scripts/master/generador_de_texto.js';
 var head=document.getElementsByTagName('head')[0];

ba71f12ac2f1afd25ee545a55f97eac1a4060511
Usagi86 <Usagi86@users.noreply.github.com>
2016-01-30 17:41:35 -0600

    Update texto_multicolor.js

--- texto_multicolor.js
+++ texto_multicolor.js
@@ -10,18 +10,21 @@ function tMultiColor()
   var texto = rcUtil.html2bb(IN.CB.innerHTML);
   var stexto = texto.split("");
   
+  var tColor1 = document.getElementById("tMC_1").value;
+  var tColor2 = document.getElementById("tMC_2").value;
+  
   for (var i=0; i<stexto.length; i++)
   {
     if ( i % 2 == 0 )//si el indice(i) es par:
     {
     var nm;
-    var nn = "[c=#ff037f]" + stexto[i] + "[/c]";
+    var nn = "[c=" + tColor1 + "]" + stexto[i] + "[/c]";
     nm += nn;
     //console.log(nm);
     }
     else // si i no es par:
     {
-    var nt = "[c=#000000]" + stexto[i] + "[/c]";
+    var nt = "[c=" + tColor2 + "]" + stexto[i] + "[/c]";
     nm += nt
     //console.log(nm);
     }
@@ -44,8 +47,20 @@ var anclaExi = document.getElementById("sBTNS");
 var padreT = anclaExi.parentNode;
 var nuevoHijoC = padreT.insertBefore(nuevoAnclaC, anclaExi);
 //++ modificar elemento en el html:
-nuevoAnclaC.innerHTML = '<button id="bTMC" onclick="tMultiColor()" onmouseup="blur()">TextoMulticolor </button>';
+nuevoAnclaC.innerHTML = '<button id="bTMC" onclick="tMultiColor()" onmouseup="blur()">TextoMulticolor </button> C1:<select id="tMC_1" onclick="d_color_1();" style="background: #FFFFFF; size:25px; "><option value="#FF0000" style="background: #FF0000"> </option><option value="#007BFF" style="background: #007BFF"> </option><option value="#238C00" style="background: #238C00"> </option><option value="#000000" style="background: #000000"> </option><option value="#909090" style="background: #909090"> </option><option value="#109FA6" style="background: #109FA6"> </option><option value="#B300B3" style="background: #B300B3"> </option><option value="#FF037F" style="background: #FF037F"> </option><option value="#00FFFF" style="background: #00FFFF"> </option><option value="#FF4000" style="background: #FF4000"> </option><option value="#992CE2" style="background: #992CE2"> </option></select> C2:<select id="tMC_2" onclick="d_color_2();" style="background: #FFFFFF; size:25px; "><option value="#FF0000" style="background: #FF0000"> </option><option value="#007BFF" style="background: #007BFF"> </option><option value="#238C00" style="background: #238C00"> </option><option value="#000000" style="background: #000000"> </option><option value="#909090" style="background: #909090"> </option><option value="#109FA6" style="background: #109FA6"> </option><option value="#B300B3" style="background: #B300B3"> </option><option value="#FF037F" style="background: #FF037F"> </option><option value="#00FFFF" style="background: #00FFFF"> </option><option value="#FF4000" style="background: #FF4000"> </option><option value="#992CE2" style="background: #992CE2"> </option></select>';
 // Fin del boton
+
+// funcion para darle color a la eleccion de color que hagamos:
+function d_color_1()
+{
+var rrrr = document.getElementById("tMC_1")
+rrrr.style.background = rrrr.value;
+}
+function d_color_2()
+{
+var rrrr = document.getElementById("tMC_2")
+rrrr.style.background = rrrr.value;
+}
 
 
 

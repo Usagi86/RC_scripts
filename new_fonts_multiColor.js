84020c5c01a4b94d6406f354a08fac887fee02e7
Usagi86 <Usagi86@users.noreply.github.com>
2016-02-01 00:23:16 -0600

    Create new_fonts_multiColor.js

---
+++ new_fonts_multiColor.js
@@ -0,0 +1,13 @@
+//
+//
+//
+//
+/**********************************************
+    Nuevo Script
+    en donde 'uniré' en un solo codigo
+    el script del "Generador_de_texto"
+    y el script del "Texto_multi_color".
+    para que se pueda escribir tanto a colores
+    con letras normales, como con las letras
+    de símbolos.
+***********************************************/

eaf3fae9941715e79b5d51bb7fc7933f3737e770
Usagi86 <Usagi86@users.noreply.github.com>
2016-02-06 01:23:35 -0600

    Update gradientes_text.js

--- gradientes_text.js
+++ gradientes_text.js
@@ -86,35 +86,28 @@ function gradient (thetext,thecolors)
 	console.log(numchars);
 	
 //////////////////////////////////////////////////////////////////	
-    for (i=0; i<numchars; ++i) 
+	for (i=0; i<numchars; ++i) 
 	{
-      lci = lowcolorindex(i, numchars, numcolors);
-      hci = hicolorindex(i, numchars, numcolors, lci);
-      rr = Math.round(interpolate( lci/(numcolors-1), colors.codes[lci].r, hci/(numcolors-1), colors.codes[hci].r, i/(numchars-1)));
-      gg = Math.round(interpolate( lci/(numcolors-1), colors.codes[lci].g, hci/(numcolors-1), colors.codes[hci].g, i/(numchars-1)));
-      bb = Math.round(interpolate( lci/(numcolors-1), colors.codes[lci].b, hci/(numcolors-1), colors.codes[hci].b, i/(numchars-1)));
-
-      
-	//  document.write(thetext.charAt(i).fontcolor("#"+tohex[rr]+tohex[gg]+tohex[bb]));
-	
-	
-	//var salida = thetext.charAt(i).fontcolor("#"+tohex[rr]+tohex[gg]+tohex[bb]);
+      	lci = lowcolorindex(i, numchars, numcolors);
+      	hci = hicolorindex(i, numchars, numcolors, lci);
+      	rr = Math.round(interpolate( lci/(numcolors-1), colors.codes[lci].r, hci/(numcolors-1), colors.codes[hci].r, i/(numchars-1)));
+      	gg = Math.round(interpolate( lci/(numcolors-1), colors.codes[lci].g, hci/(numcolors-1), colors.codes[hci].g, i/(numchars-1)));
+      	bb = Math.round(interpolate( lci/(numcolors-1), colors.codes[lci].b, hci/(numcolors-1), colors.codes[hci].b, i/(numchars-1)));
+	/////
 	var salida = "[c=#" + tohex[rr] + tohex[gg] + tohex[bb] + "]" + thetext.charAt(i) + "[/c]"
-  nT += salida
-  nR = nT.replace("undefined", "");
-	//getElementById("salida_t").innerHTML = salida;
-    
-    
+  	nT += salida
+  	nR = nT.replace("undefined", "");
 	}
 
-  console.log(nR);
   //aqui debe ir el envio de datos:
-  BL.sendMessage(TB.aT,rcUtil.html2bb(nR));
+  var tN = "[b]"+nR+"[/b]";
+  BL.sendMessage(TB.aT,rcUtil.html2bb(tN));
   IN.CI.document.body.innerHTML="";
   nT="";
   nR="";
 }
 
+// Aqui tomamos el texto y se lo pasamos a la funcion de arriba:
 var e_text = IN.CI.document.body.innerHTML;
 var r_t = e_text.replace("<br>","");
 gradient(r_t,"EA112F 6F6EF1 02B5F3 00FF00 10650D 164CB2");

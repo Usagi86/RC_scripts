e314ebae63893aa0c2bafce674b65cb1539243d2
Usagi86 <Usagi86@users.noreply.github.com>
2018-03-03 21:49:28 -0600

    Update uppercase.js

--- uppercase.js
+++ uppercase.js
@@ -1,80 +1,77 @@
 
 // Script UPPERCASE intercalado:
 
+// si el ultimo caracter de IN.CB.textContent == . >> entonces cambia toda la linea de texto.
 
 function cambiarTexto()
 {
-var texto = IN.CB.innerHTML;
-var stexto = texto.split("");
+	var texto = IN.CB.innerHTML;
+	var stexto = texto.split("");
 	if (controlMayus==true)
 	{
-for (var i=0; i<stexto.length; i++)
-{
-  if ( i % 2 == 0 ) 
-  {//si el indice es impar...
-		var textoImpar = stexto[i].toUpperCase()
-		textoImpar += stexto[i+1];// le sumamos los caracteres Pares.
+		for (var i=0; i<stexto.length; i++)
+		{
+			if ( i % 2 == 0 ) 
+			{//si el indice es impar...
+				var textoImpar = stexto[i].toUpperCase()
+				textoImpar += stexto[i+1];// le sumamos los caracteres Pares.
 
-		var textoArray = new Array(textoImpar);//metemos eso en un array.
-		var unirT;
-		unirT += textoArray.toString();//convertimos el array en string.
-		var unirT2 = unirT.replace("undefined", "");
-		//console.log(unirT2);
-  }
+				var textoArray = new Array(textoImpar);//metemos eso en un array.
+				var unirT;
+				unirT += textoArray.toString();//convertimos el array en string.
+				var unirT2 = unirT.replace("undefined", "");
+				//console.log(unirT2);
+			}
+		}
+		
+		var textoUnido = unirT2.replace("undefined", "");
+
+		//console.log(textoUnido);
+		//BL.sendMessage(TB.aT,rcUtil.html2bb(textoUnido));
+		unirT="";
+		if ( IN.CB.textContent.endsWith(".") == true )
+		{
+			var textoArray2 = new Array(textoUnido);
+			var unirT3;
+			unirT3 += textoArray2.toString();
+			IN.CB.innerHTML = unirT3.replace("undefined", "").replace("&NbSp;", " ").replace(/&nBsP;/g, " ");//reemplazamos el texto quese muestra.
+			//console.log(unirT3.replace("undefined", ""));
+			IN.doFocus();
+		}
+	}	
 }
-	
-  var textoUnido = unirT2.replace("undefined", "");
-
-  //console.log(textoUnido);
-	//BL.sendMessage(TB.aT,rcUtil.html2bb(textoUnido));
-	unirT="";
-  if ( IN.CB.textContent.endsWith(".") == true )
-	{
-	var textoArray2 = new Array(textoUnido);
-  var unirT3;
-	unirT3 += textoArray2.toString();
-	IN.CB.innerHTML = unirT3.replace("undefined", "").replace("&NbSp;", " ").replace(/&nBsP;/g, " ");//reemplazamos el texto quese muestra.
-  //console.log(unirT3.replace("undefined", ""));
-	IN.doFocus();
-  }
-}	
-}
-
 
 // bloque para el archivo que llama a los demás codigos:
 var elemento = CI.window.document.getElementById("B");
 var controlMayus = false;
 elemento.addEventListener( "keyup", function()
-		{
-		  myTe = IN.CB.textContent;
+	{
+		myTe = IN.CB.textContent;
 
-		  if ( myTe.search(/\056/) > -1 )
-			{
-				cambiarTexto();
-			}
-		})
+		if ( myTe.search(/\056/) > -1 )
+		{
+			cambiarTexto();
+		}
+	})
 
 // Funcion para el boton que activa y desactiva:
 function mM()
 {
   var bmM = document.getElementById("boton_mM")
   if ( bmM.style.background == "rgb(250, 250, 250) none repeat scroll 0% 0%" )
-    {
+   {
 		//activar:
-      bmM.style.background = "rgb(50,250,50)";
-      bmM.textContent = "M";
-			controlMayus = true;
-			IN.doFocus();
+		bmM.style.background = "rgb(50,250,50)";
+		bmM.textContent = "M";
+		controlMayus = true;
+		IN.doFocus();
     }
   else
     {
 		//desactivar:
-      bmM.style.background = "rgb(250,250,250)";
-      bmM.textContent = "m";
-			controlMayus = false;
-			IN.doFocus();
+		bmM.style.background = "rgb(250,250,250)";
+		bmM.textContent = "m";
+		controlMayus = false;
+		IN.doFocus();
     }
-  
 }
-
-
